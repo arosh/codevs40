4daafb2a79472a0c7b9cd0b4d5ac95edcce434a7
Sho Iizuka <sho.i518@gmail.com>
2014-12-26 16:34:20 +0900

    UIを分離

--- CodeVS4/MainWindow.xaml.cs
+++ CodeVS4/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace CodeVS4
+namespace CodeVS4UI
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
